cee33df84b36f2c126c79599ab858f394fda784e
Ahmed Faraj <148560615+Ahmad-Faraj@users.noreply.github.com>
2024-05-31 14:14:44 +0300

    Add files via upload

---
+++ Exam Contest SCU/Day 1 Contest/A. elbedaia/Problem_Solving.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define ull unsigned long long int
+#define ld long double
+#define ll long long
+#define PI acos(-1)
+#define endl "\n"
+#define all(x) x.begin(), x.end()
+#define dpp(arr, val) memset(dp, -1, sizeof(dp))
+#define fixed(n) cout << fixed << setprecision(n)
+#define loop(i, n) for (int i = 0; i < (int)n; i++)
+
+const int N = 1e7;
+const double EPS = 1e-6;
+const int MOD = 1000000007;
+const int OO = 0x3f3f3f3f;
+int dx[] = {1, -1, 0, 0};
+int dy[] = {0, 0, 1, -1};
+
+void Ahmed_Faraj()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+void solve()
+{
+    string s;
+    cin >> s;
+    char arr[] = {'h', 'e', 'l', 'l', 'o'};
+    bool ok = 0;
+    int i = 0;
+    for (char c : s)
+    {
+        if (c == arr[i])
+        {
+            i++;
+        }
+        if (i == 5)
+        {
+            ok = 1;
+            break;
+        }
+    }
+    if (ok)
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+}
+
+signed main()
+{
+    Ahmed_Faraj();
+    int test = 1;
+    // cin >> test;
+    for (int tc = 1; tc <= test; tc++)
+    {
+        // cout << "Case " << tc << ": ";
+        solve();
+    }
+    return 0;
+}
