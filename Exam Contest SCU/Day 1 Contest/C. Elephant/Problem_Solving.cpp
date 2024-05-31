83de1226b38c7e9f4c51e88f8fcd4a118fe1d4e0
Ahmed Faraj <148560615+Ahmad-Faraj@users.noreply.github.com>
2024-05-31 14:17:18 +0300

    Add files via upload

---
+++ Exam Contest SCU/Day 1 Contest/C. Elephant/Problem_Solving.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define ull unsigned long long int
+#define ld long double
+#define ll long long
+#define PI acos(-1)
+#define endl "\n"
+#define all(x) x.begin(), x.end()
+#define dpp(arr, val) memset(dp, -1, sizeof(dp))
+#define fixed(n) cout << fixed << setprecision(n)
+#define loop(i, n) for (int i = 0; i < (int)n; i++)
+
+const int N = 1e7;
+const double EPS = 1e-6;
+const int MOD = 1000000007;
+const int OO = 0x3f3f3f3f;
+int dx[] = {1, -1, 0, 0};
+int dy[] = {0, 0, 1, -1};
+
+void Ahmed_Faraj()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+void solve()
+{
+    long long x = 0;
+    long long i = 0;
+    cin >> x;
+    while (x > 0)
+    {
+        x = x - 5;
+        ++i;
+    }
+    cout << i << endl;
+}
+
+signed main()
+{
+    Ahmed_Faraj();
+    int test = 1;
+    // cin >> test;
+    for (int tc = 1; tc <= test; tc++)
+    {
+        // cout << "Case " << tc << ": ";
+        solve();
+    }
+    return 0;
+}
